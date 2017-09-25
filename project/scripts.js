13c99ef91780cbccd9b5bab5480518194c40d468
danielhkim92 <danielhkim92@gmail.com>
2017-09-25 16:48:25 -0500

    metrics

---
+++ project/scripts.js
@@ -0,0 +1,31 @@
+console.log('hi')
+
+class Tomagatchi {
+	constructor(name, birthday, gender,){
+	this.name = name;
+	this.birthday = birthday;
+	this.age = 0;
+	this.gender = gender;
+	this.hunger = 1;
+	this.sleepiness = 1;
+	this.boredom = 1;
+}
+	
+}
+
+const charmander = new Tomagatchi('Charmander', '7/11', 'male')
+
+const $div = $('<div>').addClass('Tomagatchi')
+$div.appendTo($('body'))
+
+const $hunger = $('<button>').attr('id', 'Hunger')
+$hunger.text('Hunger')
+$hunger.appendTo($('body'))
+
+const $light = $('<button>').attr('id', 'Lights')
+$light.text('Lights')
+$light.appendTo($('body'))
+
+const $play = $('<button>').attr('id', 'Play')
+$play.text('Play')
+$play.appendTo($('body'))
