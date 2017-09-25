7886d3a3325b8753b354613801e6f7af8d5bd52c
danielhkim92 <danielhkim92@gmail.com>
2017-09-25 15:33:16 -0500

    class

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log('hi')
+
+class Tomagatchi {
+	constructor(name, birthday, gender,){
+	this.name = name;
+	this.birthday = birthday;
+	this.gender = gender;
+}
+}
+
+const charmander = new Tomagatchi('Charmander', '7/11/92', 'male')
