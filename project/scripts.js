d8417d7215c34403f62744f33654500fab659406
danielhkim92 <danielhkim92@gmail.com>
2017-09-29 09:04:26 -0500

    nice

--- project/scripts.js
+++ project/scripts.js
@@ -1,43 +1,92 @@
 console.log('hi')
 
+
 class Tomagatchi {
 	constructor(name, birthday, gender,){
 	this.name = name;
 	this.birthday = birthday;
-	this.age = 0;
 	this.gender = gender;
-	this.hunger = 1;
-	this.sleepiness = 1;
-	this.boredom = 1;
-}
-	
+	this.age = 0;
+	this.hunger = 0;
+	this.sleepiness = 0;
+	this.boredom = 0;
+  }
 }
 
 const charmander = new Tomagatchi('Charmander', '7/11', 'male')
 
-const $div = $('<div>').addClass('Tomagatchi')
-$div.appendTo($('body'))
 
-const $hunger = $('<button>').attr('id', 'Hunger')
-$hunger.text('Hunger')
-$hunger.appendTo($('body'))
 
-const $light = $('<button>').attr('id', 'Lights')
-$light.text('Lights')
-$light.appendTo($('body'))
 
-const $play = $('<button>').attr('id', 'Play')
-$play.text('Play')
-$play.appendTo($('body'))
-let time = 0
-const setTimer = () => {
+$('#hunger').on('click', () =>{
+	$('#hungry').text('HUNGER: ' + charmander.hunger --);
+		 if(charmander.hunger<0){
+         charmander.hunger=0;  
+    }
+})
+
+$('#play').on('click', () => {
+	$('#boredom').text('BOREDOM: ' + charmander.boredom --);
+	charmander.boredom <= 0
+		 if(charmander.boredom<0){
+         charmander.boredom=0;  
+    }
+})
+
+$('#lights').on('click', () =>{
+ 	$('body').toggleClass("darkness");
+})
+
+$('#rest').on('click', () => {
+	$('#sleepiness').text('SLEEPINESS: ' + charmander.sleepiness --);
+	 if(charmander.sleepiness<0){
+         charmander.sleepiness=0;  
+    }
+})
+
+const setClockAge = () => {
 	const timer = setInterval(() => {
-		time++
-
-		// if(time === 0){
-		// 	clearInterval(timer)
-		// 	round++;
-			// $('modal-hidden'.toggleClass('.modal')
+		$('#age').text('AGE: ' + charmander.age ++);
+		if(charmander.age ===3){
+			$('#tomagatchi').css('background-image', 'url("https://cdn.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/250px-006Charizard.png")');
+		}
+		if(charmander.age===6){
+			$('#tomagatchi').css('background-image', 'url("http://media-cerulean.cursecdn.com/attachments/5/859/mega_charizard.png")');
+		}
+		if(charmander.age===9){
+			$('#tomagatchi').css('background-image', 'url("https://i.pinimg.com/originals/64/1a/ea/641aea0ebb57673d02a2cc3b6cee2821.jpg")');
+		}
+	}, 2000)}
 	
-		$('#timer').text(`timer: ${time}second old`);
-	}, 1000)}
+
+const setClockSleep = () => {
+		const timer2 = setInterval(() => {
+			   //     $('#lights').on('click', () =>{
+ 					// window.clearInterval(timer2);}) 
+
+
+
+
+				$('#boredom').text('BOREDOM: ' + charmander.boredom ++);
+				$('#hungry').text('HUNGER: ' + charmander.hunger ++);
+				$('#sleepiness').text('SLEEPINESS: ' + charmander.sleepiness ++);
+			if (charmander.sleepiness == 11) {
+        		$('#tomagatchi').css('background-image', 'url("http://i1097.photobucket.com/albums/g358/Tim_Finnegan/013_Thumb.jpg")');
+        		window.clearInterval(timer2);}
+        	if (charmander.boredom == 11) {
+        		$('#tomagatchi').css('background-image', 'url("http://i1097.photobucket.com/albums/g358/Tim_Finnegan/013_Thumb.jpg")');
+        		window.clearInterval(timer2);
+    		}
+	
+    		if (charmander.hunger == 11) {
+        		$('#tomagatchi').css('background-image', 'url("http://i1097.photobucket.com/albums/g358/Tim_Finnegan/013_Thumb.jpg")');
+        			window.clearInterval(timer2);
+    }
+	}, 3000)}
+
+
+
+setClockSleep()
+setClockAge()
+
+
